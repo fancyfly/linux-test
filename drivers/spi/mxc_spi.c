11f6e8d02102d74356ad0d080c515909a5824f54
Nancy Chen <Nancy.Chen@freescale.com>
2008-05-14 09:34:45 -0500

    ENGR00069620 SPI suspend issue
    System can't go into low power mode due to error returned from SPI driver
    
    Signed-off-by: Nancy Chen <Nancy.Chen@freescale.com>

--- drivers/spi/mxc_spi.c
+++ drivers/spi/mxc_spi.c
@@ -1094,21 +1094,6 @@ static int mxc_spi_remove(struct platform_device *pdev)
 }
 
 #ifdef CONFIG_PM
-static int suspend_devices(struct device *dev, void *pm_message)
-{
-	pm_message_t *state = pm_message;
-
-	if (!dev->driver)
-		return 0;
-
-	if (dev->power.power_state.event != state->event) {
-		dev_warn(dev, "mismatch in pm state request\n");
-		return -1;
-	}
-
-	return 0;
-}
-
 static int spi_bitbang_suspend(struct spi_bitbang *bitbang)
 {
 	unsigned long flags;
@@ -1154,11 +1139,6 @@ static int mxc_spi_suspend(struct platform_device *pdev, pm_message_t state)
 	struct spi_master *master = platform_get_drvdata(pdev);
 	struct mxc_spi *master_drv_data = spi_master_get_devdata(master);
 	int ret = 0;
-
-	if (device_for_each_child(&pdev->dev, &state, suspend_devices) != 0) {
-		dev_warn(&pdev->dev, "suspend aborted\n");
-		return -EINVAL;
-	}
 
 	spi_bitbang_suspend(&master_drv_data->mxc_bitbang);
 	__raw_writel(MXC_CSPICTRL_DISABLE,

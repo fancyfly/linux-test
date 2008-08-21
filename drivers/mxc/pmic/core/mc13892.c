c191957be0046bdac6cf0572e7903501275b63f8
Shen Yong <b00984@freescale.com>
2008-08-21 11:07:41 +0800

    ENGR00088655 mc13892: disable charge by default
    disable charge by default
    
    Signed-off-by: Kevin Shen <b00984@freescale.com>

--- drivers/mxc/pmic/core/mc13892.c
+++ drivers/mxc/pmic/core/mc13892.c
@@ -97,6 +97,8 @@ int pmic_init_registers(void)
 	CHECK_ERROR(pmic_write(REG_INT_MASK0, 0xFFFFFF));
 	CHECK_ERROR(pmic_write(REG_INT_STATUS0, 0xFFFFFF));
 	CHECK_ERROR(pmic_write(REG_INT_STATUS1, 0xFFFFFF));
+	/* disable auto charge */
+	CHECK_ERROR(pmic_write(REG_CHARGE, 0xB40003));
 
 	return PMIC_SUCCESS;
 }

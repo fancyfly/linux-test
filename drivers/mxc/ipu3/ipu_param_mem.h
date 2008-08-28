bd2b15dece123506149ddc4ee6bdd4064ec52e6e
Jason Chen <b02280@freescale.com>
2008-08-28 10:11:19 +0800

    ENGR00069669 MX51 DP FG display to small width fail
    Change burst size to 32 for UYVY and YUYV format.
    
    Signed-off-by: Jason Chen <b02280@freescale.com>

--- drivers/mxc/ipu3/ipu_param_mem.h
+++ drivers/mxc/ipu3/ipu_param_mem.h
@@ -210,12 +210,12 @@ static inline void _ipu_ch_param_init(int ch,
 	case IPU_PIX_FMT_UYVY:
 		ipu_ch_param_set_field(&params, 0, 107, 3, 3);	/* bits/pixel */
 		ipu_ch_param_set_field(&params, 1, 85, 4, 0xA);	/* pix format */
-		ipu_ch_param_set_field(&params, 1, 78, 7, 15);	/* burst size */
+		ipu_ch_param_set_field(&params, 1, 78, 7, 31);	/* burst size */
 		break;
 	case IPU_PIX_FMT_YUYV:
 		ipu_ch_param_set_field(&params, 0, 107, 3, 3);	/* bits/pixel */
 		ipu_ch_param_set_field(&params, 1, 85, 4, 0x8);	/* pix format */
-		ipu_ch_param_set_field(&params, 1, 78, 7, 15);	/* burst size */
+		ipu_ch_param_set_field(&params, 1, 78, 7, 31);	/* burst size */
 		break;
 	case IPU_PIX_FMT_YUV420P2:
 	case IPU_PIX_FMT_YUV420P:

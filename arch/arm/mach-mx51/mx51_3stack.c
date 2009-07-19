21ee47e52b1ce33ad7e1ac5ac596638fee69afff
Lily Zhang <r58066@freescale.com>
2009-07-19 17:20:16 +0800

    ENGR00113662 enable coin cell charger
    Enable coin-cell charger so that coin-cell battery can be used as
    backup battery after power down. To keep RTC alive after power off,
    ensure the voltage of coin-cell battery is higher than 1.8V
    
    Signed-off-by: Lily Zhang <r58066@freescale.com>

--- arch/arm/mach-mx51/mx51_3stack.c
+++ arch/arm/mach-mx51/mx51_3stack.c
@@ -103,10 +103,27 @@ static void power_on_evt_handler(void)
 	pr_info("pwr on event1 is received \n");
 }
 
+/* Coin cell charger enable */
+#define CIONCHEN_LSH   23
+#define CIONCHEN_WID   1
+/* Coin cell charger voltage setting */
+#define VCOIN_LSH      20
+#define VCOIN_WID      3
+
+/* Coin Charger voltage */
+#define VCOIN_2_5V     0x0
+#define VCOIN_2_7V     0x1
+#define VCOIN_2_8V     0x2
+#define VCOIN_2_9V     0x3
+#define VCOIN_3_0V     0x4
+#define VCOIN_3_1V     0x5
+#define VCOIN_3_2V     0x6
+#define VCOIN_3_3V     0x7
+
 static int __init mc13892_reg_int(void)
 {
 	int i = 0;
-	unsigned int value;
+	unsigned int value, register_mask;
 	struct regulator *regulator;
 	struct regulator *gp;
 	struct regulator *lp;
@@ -197,6 +214,11 @@ static int __init mc13892_reg_int(void)
 	pmic_read_reg(REG_POWER_CTL0, &value, 0xffffff);
 	value |= 0x000010;
 	pmic_write_reg(REG_POWER_CTL0, value, 0xffffff);
+
+       /* Enable coin cell charger */
+       value = BITFVAL(CIONCHEN, 1) | BITFVAL(VCOIN, VCOIN_3_0V);
+       register_mask = BITFMASK(CIONCHEN) | BITFMASK(VCOIN);
+       pmic_write_reg(REG_POWER_CTL0, value, register_mask);
 
 	return 0;
 }

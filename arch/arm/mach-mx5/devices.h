ca007b01a6b25e0d59693cdcce656475fb735e09
Xinyu Chen <xinyu.chen@freescale.com>
2010-11-02 13:49:42 +0800

    MX5x MSL: Add pmem and android usb gadget platform device
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- arch/arm/mach-mx5/devices.h
+++ arch/arm/mach-mx5/devices.h
@@ -94,3 +94,6 @@ extern struct platform_device pm_device;
 extern struct platform_device fixed_volt_reg_device;
 extern struct platform_device mxc_zq_calib_device;
 extern int z160_revision;
+extern struct platform_device mxc_android_pmem_device;
+extern struct platform_device mxc_android_pmem_gpu_device;
+extern struct platform_device android_usb_device;

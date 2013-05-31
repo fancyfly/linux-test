d7a3212c58526ac09ebd321ac0a64af71d503fe4
Lin Fuzhen <fuzhen.lin@freescale.com>
2013-05-31 18:23:14 +0800

    ENGR00265143-2 add camera sensors driver back
    Camera feature is  the key feature that Android fastboot soultion
    
    Signed-off-by: Lin Fuzhen <fuzhen.lin@freescale.com>

--- arch/arm/mach-mx6/board-mx6q_sabresd.c
+++ arch/arm/mach-mx6/board-mx6q_sabresd.c
@@ -1919,13 +1919,13 @@ static void __init mx6_sabresd_board_init(void)
 			imx6q_add_ipuv3fb(i, &sabresd_fb_data[i]);
 
 	imx6q_add_vdoa();
-	/* imx6q_add_mipi_dsi(&mipi_dsi_pdata); */
+	imx6q_add_mipi_dsi(&mipi_dsi_pdata);
 	imx6q_add_lcdif(&lcdif_data);
 	imx6q_add_ldb(&ldb_data);
 	imx6q_add_v4l2_output(0);
 	imx6q_add_v4l2_capture(0, &capture_data[0]);
 	imx6q_add_v4l2_capture(1, &capture_data[1]);
-	/* imx6q_add_mipi_csi2(&mipi_csi2_pdata); */
+	imx6q_add_mipi_csi2(&mipi_csi2_pdata);
 	imx6q_add_imx_snvs_rtc();
 
 	if (1 == caam_enabled)

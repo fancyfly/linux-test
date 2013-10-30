5036a9075cfa1ebf636e2a00badffb322038da1a
Jack Lee <jack.lee@freescale.com>
2013-10-30 16:14:46 +0800

    ENGR00285446-2 [MX28] SAIF: Bit Shift in SAIF RX Data
    When SAIF is in functional operation, BYPASS_SAIF should be 0.
    Currently, SAIF.1 is not explicitly configured. As a result, SAIF.0
    and SAIF.1 are using two different clock sources. ( ref_pll and ref_xtal )
    Based on the experiment of different clock settings, we can identify
    bit shift error in some configurations. The common factor of these
    configurations are in different clock source.
    Thus, during clock tree setup, SAIF.1 should be created to have the same
    settings of SAIF.0. When both SAIF are using ref_pll clock source,
    bit shift issue will not happen.
    
    Signed-off-by: Jack Lee <jack.lee@freescale.com>

--- arch/arm/plat-mxs/include/mach/device.h
+++ arch/arm/plat-mxs/include/mach/device.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2012 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright (C) 2009-2013 Freescale Semiconductor, Inc. All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -137,6 +137,7 @@ struct mxs_audio_platform_data {
 	int intr_id_hp;
 	int ext_ram;
 	struct clk *saif_mclock;
+	struct clk *saif_mclock1;
 
 	int hp_irq;
 	int (*hp_status) (void);

ae0fc3389cae5ff5d5ada6fbe41401f8a7b0f55d
Xianzhong <b07117@freescale.com>
2014-04-21 20:06:46 +0800

    Integrate gpu 5.0.11 release kernel driver for Android
    This is a temporary integration to verify 5.x gpu quality,
    The integration is based on 5.0.9 for jb4.3_1.1.0-ga.
    
    Signed-off-by: Xianzhong <b07117@freescale.com>
    Acked-by: Jason Liu

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_debug.h
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_debug.h
@@ -48,13 +48,14 @@ typedef va_list gctARGUMENTS;
     va_arg(Arguments, Type)
 
 #define gcmkDECLARE_LOCK(__spinLock__) \
-    static DEFINE_SPINLOCK(__spinLock__);
+    static DEFINE_SPINLOCK(__spinLock__); \
+    unsigned long __spinLock__##flags = 0;
 
 #define gcmkLOCKSECTION(__spinLock__) \
-    spin_lock(&__spinLock__)
+    spin_lock_irqsave(&__spinLock__, __spinLock__##flags)
 
 #define gcmkUNLOCKSECTION(__spinLock__) \
-    spin_unlock(&__spinLock__)
+    spin_unlock_irqrestore(&__spinLock__, __spinLock__##flags)
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,24)
 #   define gcmkGETPROCESSID() \
@@ -91,7 +92,7 @@ typedef va_list gctARGUMENTS;
     snprintf(Destination, Size, Message, Value1, Value2, Value3)
 
 #define gcmkVSPRINTF(Destination, Size, Message, Arguments) \
-    vsnprintf(Destination, Size, Message, *(va_list *) &Arguments)
+    vsnprintf(Destination, Size, Message, *((va_list*)Arguments))
 
 #define gcmkSTRCAT(Destination, Size, String) \
     strncat(Destination, String, Size)

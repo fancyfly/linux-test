5a7054f9ac840f3262ce69d86956860ed01d3d41
Jay Tu <jay.tu@freescale.com>
2014-07-29 17:42:42 +0800

    MA-5870 Enlarge DMA size for triple display
    GPU command buffer needs larger dma for triple display.
    
    Signed-off-by: Jay Tu <jay.tu@freescale.com>

--- arch/arm/plat-mxc/include/mach/memory.h
+++ arch/arm/plat-mxc/include/mach/memory.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2012 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2004-2014 Freescale Semiconductor, Inc. All Rights Reserved.
  */
 
 /*
@@ -65,7 +65,7 @@
 #else
 
 #ifndef CONFIG_ZONE_DMA
-#define CONSISTENT_DMA_SIZE     (64 * SZ_1M)
+#define CONSISTENT_DMA_SIZE     (128 * SZ_1M)
 #elif defined(CONFIG_ARCH_MX5) || defined(CONFIG_ARCH_MX6)
 #define ARM_DMA_ZONE_SIZE	(184 * SZ_1M)
 #define CONSISTENT_DMA_SIZE	ARM_DMA_ZONE_SIZE

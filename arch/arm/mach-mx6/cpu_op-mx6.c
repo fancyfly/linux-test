68c81de1878b50311acf92e7c7befa86a5493792
Peng Fushi <fushi.peng@freescale.com>
2014-12-05 17:16:24 +0800

    MLK-9966 arm: imx: update operating point for imx6Q
    Update the imx6Q cpu operating points to comply with the latest
    published datasheet. Latest i.MX6Q datasheet of Rev.3, 02/2014
    updates the 792MHz setpoint's min voltage from 1.125V to 1.15V, Add a
    25mV margin to cover the board IR drop, here use 1.175V for 792MHz to
    match datesheet
    
    Signed-off-by: Peng Fushi <fushi.peng@freescale.com>

--- arch/arm/mach-mx6/cpu_op-mx6.c
+++ arch/arm/mach-mx6/cpu_op-mx6.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2013 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright (C) 2010-2014 Freescale Semiconductor, Inc. All Rights Reserved.
  */
 
 /*
@@ -53,7 +53,7 @@ static struct cpu_op mx6q_cpu_op_1_2G[] = {
 	 .pu_voltage = 1175000,
 	 .soc_voltage = 1175000,
 #endif
-	 .cpu_voltage = 1150000,},
+	 .cpu_voltage = 1175000,},
 #ifdef CONFIG_MX6_VPU_352M
 	/*pll2_pfd_400M will be fix on 352M,to avoid modify other code
 	which assume ARM clock sourcing from pll2_pfd_400M, change cpu
@@ -97,7 +97,7 @@ static struct cpu_op mx6q_cpu_op_1G[] = {
 	 .pu_voltage = 1175000,
 	 .soc_voltage = 1175000,
 #endif
-	 .cpu_voltage = 1150000,},
+	 .cpu_voltage = 1175000,},
 #ifdef CONFIG_MX6_VPU_352M
 	/*pll2_pfd_400M will be fix on 352M,to avoid modify other code
 	which assume ARM clock sourcing from pll2_pfd_400M, change cpu
@@ -133,7 +133,7 @@ static struct cpu_op mx6q_cpu_op[] = {
 	 .pu_voltage = 1175000,
 	 .soc_voltage = 1175000,
 #endif
-	 .cpu_voltage = 1150000,},
+	 .cpu_voltage = 1175000,},
 #ifdef CONFIG_MX6_VPU_352M
 	/*pll2_pfd_400M will be fix on 352M,to avoid modify other code
 	which assume ARM clock sourcing from pll2_pfd_400M, change cpu

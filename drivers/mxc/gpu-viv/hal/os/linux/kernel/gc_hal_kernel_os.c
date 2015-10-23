fbf2bbb709e90bf26e391aaf7390c4524d4a7ba1
Xianzhong <b07117@freescale.com>
2015-10-23 13:12:16 +0800

    fix gpu false hang without delay sync
    Revert "bypass gckOS_Delay in gpu kernel driver"
    
    This reverts commit 665672bf8dfe11f41688dc7f77e55e3c8a2803a5.
    
    Signed-off-by: Xianzhong <b07117@freescale.com>

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_os.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_os.c
@@ -3356,7 +3356,6 @@ gckOS_Delay(
 {
     gcmkHEADER_ARG("Os=0x%X Delay=%u", Os, Delay);
 
-#if 0
     if (Delay > 0)
     {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 28)
@@ -3367,7 +3366,6 @@ gckOS_Delay(
         msleep(Delay);
 #endif
     }
-#endif
 
     /* Success. */
     gcmkFOOTER_NO();

eb6a5611dfe8538f83e884d1832001c88dc950af
Shenwei Wang <shenwei.wang@freescale.com>
2016-02-01 22:17:01 -0600

    Enabled the Freescale ethernet driver (fec_main) on Zebu.
    Signed-off-by: Shenwei Wang <shenwei.wang@freescale.com>

--- drivers/net/ethernet/freescale/fec.h
+++ drivers/net/ethernet/freescale/fec.h
@@ -20,7 +20,7 @@
 
 #if defined(CONFIG_M523x) || defined(CONFIG_M527x) || defined(CONFIG_M528x) || \
     defined(CONFIG_M520x) || defined(CONFIG_M532x) || \
-    defined(CONFIG_ARCH_MXC) || defined(CONFIG_SOC_IMX28)
+    defined(CONFIG_ARCH_MXC) || defined(CONFIG_SOC_IMX28) || defined(CONFIG_ARCH_NXP_IMX8)
 /*
  *	Just figures, Motorola would have to change the offsets for
  *	registers in the same peripheral device on different models

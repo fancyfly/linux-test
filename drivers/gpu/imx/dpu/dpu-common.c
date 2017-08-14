8ae2be71f30f25cb2ccb8a1a1cb6cbf3074473ab
Meng Mingming <mingming.meng@nxp.com>
2017-08-14 13:33:54 +0800

    MLK-15321-1 gpu: imx: dpu: Add dpu blit engine device
    Implement Blt engine as DRM renderer.
    Add dpu blit engine device.
    
    Signed-off-by: Meng Mingming <mingming.meng@nxp.com>

--- drivers/gpu/imx/dpu/dpu-common.c
+++ drivers/gpu/imx/dpu/dpu-common.c
@@ -837,6 +837,9 @@ static struct dpu_platform_reg client_reg[] = {
 			.stream_id = 1,
 		},
 		.name = "imx-dpu-crtc",
+	}, {
+		.pdata = { },
+		.name = "imx-drm-dpu-bliteng",
 	},
 };
 
@@ -981,21 +984,29 @@ static int dpu_add_client_devices(struct dpu_soc *dpu)
 
 	for (i = 0; i < client_num; i++) {
 		struct platform_device *pdev;
-		struct device_node *of_node;
-		bool is_disp;
+		struct device_node *of_node = NULL;
+		bool is_disp, is_bliteng;
 
-		if (devtype->has_capture)
-			is_disp = (i / 2) ? true : false;
-		else
-			is_disp = true;
+		if (devtype->has_capture) {
+			is_bliteng = (i == 4) ? true : false;
+			is_disp = (!is_bliteng) && ((i / 2) ? true : false);
+		} else {
+			is_bliteng = (i == 2) ? true : false;
+			is_disp = !is_bliteng;
+		}
 
-		/* Associate subdevice with the corresponding port node */
-		of_node = of_graph_get_port_by_id(dev->of_node, i);
-		if (!of_node) {
-			dev_info(dev, "no port@%d node in %s, not using %s%d\n",
-				 i, dev->of_node->full_name,
-				 is_disp ? "DISP" : "CSI", i % 2);
-			continue;
+		if (!is_bliteng) {
+			/*
+			 * Associate subdevice with the
+			 * corresponding port node.
+			 */
+			of_node = of_graph_get_port_by_id(dev->of_node, i);
+			if (!of_node) {
+				dev_info(dev, "no port@%d node in %s, not using %s%d\n",
+					i, dev->of_node->full_name,
+					is_disp ? "DISP" : "CSI", i % 2);
+				continue;
+			}
 		}
 
 		if (is_disp)
@@ -1009,7 +1020,8 @@ static int dpu_add_client_devices(struct dpu_soc *dpu)
 
 		pdev->dev.parent = dev;
 
-		reg[i].pdata.of_node = of_node;
+		if (!is_bliteng)
+			reg[i].pdata.of_node = of_node;
 		ret = platform_device_add_data(pdev, &reg[i].pdata,
 					       sizeof(reg[i].pdata));
 		if (!ret)

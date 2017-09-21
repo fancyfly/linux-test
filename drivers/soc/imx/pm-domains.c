eb4ed9b05913338e12492768981cd5bbaef290f4
Leonard Crestez <leonard.crestez@nxp.com>
2017-09-21 20:03:13 +0300

    MLK-16225: imx8 pm: Defer sc api initialization
    The current imx8 pm code initializes sc api early and fails if it's not
    yet available. This can be a problem if sc api calls go through a
    paravirtualized interface.
    
    This patch moves ipc initialization to the attach_dev function and
    returns -EPROBE_DEFER if not yet available. This is vaguely similar to
    what clk does and not very intrusive.
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>

--- drivers/soc/imx/pm-domains.c
+++ drivers/soc/imx/pm-domains.c
@@ -33,6 +33,29 @@
 
 static sc_ipc_t pm_ipc_handle;
 
+static int imx8_pm_init_ipc(void)
+{
+	sc_err_t sci_err;
+	uint32_t mu_id;
+
+	if (pm_ipc_handle)
+		return 0;
+
+	sci_err = sc_ipc_getMuID(&mu_id);
+	if (sci_err != SC_ERR_NONE) {
+		pr_info("%s: Cannot obtain MU ID\n", __func__);
+		return -EPROBE_DEFER;
+	}
+
+	sci_err = sc_ipc_open(&pm_ipc_handle, mu_id);
+	if (sci_err != SC_ERR_NONE) {
+		pr_info("%s: Cannot obtain IPC handle\n", __func__);
+		return -EPROBE_DEFER;
+	}
+
+	return 0;
+}
+
 static int imx8_pd_power(struct generic_pm_domain *domain, bool power_on)
 {
 	struct imx8_pm_domain *pd;
@@ -149,6 +172,20 @@ static int imx8_attach_dev(struct generic_pm_domain *genpd, struct device *dev)
 	struct of_phandle_args clkspec;
 	int rc, index, num_clks;
 
+	/*
+	 * If SC is not yet available return an error on attach.
+	 * The PM core will handle deferral.
+	 *
+	 * The PM core also ensure mutual exclusion, this is only
+	 * called with gpd_list_lock held.
+	 */
+	rc = imx8_pm_init_ipc();
+	if (rc) {
+		pr_debug("%s: pd=%s dev=%s SC not yet available, defer\n",
+				__func__, genpd->name, dev_name(dev));
+		return rc;
+	}
+
 	pd = container_of(genpd, struct imx8_pm_domain, pd);
 	INIT_LIST_HEAD(&pd->clks);
 
@@ -250,9 +287,7 @@ static int __init imx8_add_pm_domains(struct device_node *parent,
 static int __init imx8_init_pm_domains(void)
 {
 	struct device_node *np;
-	sc_err_t sci_err;
 	sc_rsrc_t rsrc_id;
-	uint32_t mu_id;
 
 	/* skip pm domains for non-SCFW system */
 	if (!of_find_compatible_node(NULL, NULL, "nxp,imx8-pd"))
@@ -288,14 +323,6 @@ static int __init imx8_init_pm_domains(void)
 		of_genpd_add_provider_simple(np, &imx8_pd->pd);
 		imx8_add_pm_domains(np, &imx8_pd->pd);
 	}
-
-	sci_err = sc_ipc_getMuID(&mu_id);
-	if (sci_err != SC_ERR_NONE) {
-		pr_info("Cannot obtain MU ID\n");
-		return sci_err;
-	}
-
-	sci_err = sc_ipc_open(&pm_ipc_handle, mu_id);
 
 	return 0;
 }
